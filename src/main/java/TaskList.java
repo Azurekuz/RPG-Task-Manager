d21d1e2474dc083a799c419d75cd99a59d129268
lwatson98 <petrie.watson@gmail.com>
2020-03-27 13:56:34 -0400

    Put in the skeleton for the TaskList class

--- src/main/java/TaskList.java
+++ src/main/java/TaskList.java
@@ -1,2 +1,24 @@
-public class TaskList {
+import java.awt.*;
+import java.util.ArrayList;
+
+public class TaskList extends TaskManager{
+        List <Task> taskList =new ArrayList<Task>();
+
+
+        public void addTask(){}
+
+    public void removeTask(int id){
+
+
+
+    }
+    public void editTask(int id){
+
+
+    }
+    public void findTask(String title, int id){
+
+
+    }
+
 }
