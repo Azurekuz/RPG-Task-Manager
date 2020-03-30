e8498e6be1cb088b805420b77ba17d6c4d763fe6
E-3P0 <elias@jwplatt.com>
2020-03-30 18:06:36 -0400

    started tests, hopefully branch worked this time

--- src/test/java/TaskTest.java
+++ src/test/java/TaskTest.java
@@ -1,8 +1,52 @@
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 
 public class TaskTest {
     @Test
     public void TaskTest(){
+        //id, title, desc, quality, timeLimit, type, complete
+        Task testTask = new Task(0, "title1","desc1",0, -1, 0, false);
+
+        //TODO test error checking/throws for invalid values (determine invalid values
+        assertEquals(0, testTask.getID());
+        assertEquals("title1", testTask.getTitle());
+        assertEquals("desc1", testTask.getDesc());
+        assertEquals(0, testTask.getQuality());
+
+        assertEquals(-1, testTask.getTimeLimit());
+        assertEquals(false, testTask.isTimed());
+
+        assertEquals(0, testTask.getTypeInt());
+        assertEquals("default", testTask.getTypeStr());
+        assertEquals(false, testTask.isComplete());
+
+        testTask.setID(1);
+        assertEquals(1, testTask.getID());
+
+        testTask.setTitle("newtitle1");
+        assertEquals("newtitle1", testTask.getTitle());
+
+        testTask.setDesc("newdesc1", testTask.getDesc());
+        assertEquals("newdesc1", testTask.getDesc());
+
+        testTask.setQuality(10);
+        assertEquals(10, testTask.getQuality());
+
+        testTask.setTimeLimit(1);
+        assertEquals(1, testTask.getTimeLimit());
+        assertEquals(true, testTask.isTimed());
+        testTask.setTimeLimit(3600);
+        assertEquals(3600, testTask.getTimeLimit());
+
+
+        testTask.setType(1);
+        assertEquals(1, testTask.getTypeInt());
+        assertEquals("main", testTask.getTypeStr());
+
+        testTask.complete();
+        assertEquals(true, testTask.isComplete());
+
 
     }
 }
