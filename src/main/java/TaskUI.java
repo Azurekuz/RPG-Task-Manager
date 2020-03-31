1492b2e98755680464f5255f29940ef965bab64d
E-3P0 <elias@jwplatt.com>
2020-03-31 14:10:24 -0400

    Started UI.

--- src/main/java/TaskUI.java
+++ src/main/java/TaskUI.java
@@ -1,2 +1,40 @@
+import java.util.Scanner;
 public class TaskUI {
+    public TaskManager taskManager;
+
+    public void commandHandler(){
+        taskManager = new TaskManager();
+        //taskManager.load();
+
+        Scanner input = new Scanner(System.in);
+        String userStr = input.nextLine();
+        while (!(userStr.equals("quit"))){
+            System.out.println("Enter your command or 'help' to see a list of commands.");
+
+            if (userStr.equals("help")){
+                System.out.println("***AVAILABLE COMMANDS***");
+                System.out.println("'help' : Displays this");
+                System.out.println("'quit' : Stops the program");
+                System.out.println();
+            }
+            //else if (){
+            //
+            //}
+            else{
+                System.out.println("***Command not recognized.***");
+            }
+
+            userStr = input.nextLine();
+        }
+
+        System.out.println("Saving and quitting...");
+        //taskManager.save();
+    }
+
+    public static void main(String args[]){
+        TaskUI taskUI = new TaskUI();
+
+        System.out.println("Welcome to RPG Task Manager!");
+        taskUI.commandHandler();
+    }
 }
