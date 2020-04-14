b6494458d53dd3311129373019a4525be7f6aa43
lwatson98 <petrie.watson@gmail.com>
2020-04-14 13:21:44 -0400

    Create Achievement and AchievementList tests in icncluded

---
+++ src/test/java/AchievementTest.java
@@ -0,0 +1,12 @@
+public class AchievementTest {
+    @Test
+
+        Achievement TestAchievement1 = new Achievement(1, "Achievement1",1, false, "New Achievement");
+        Achievement TestAchievement2 = new Achievement(2, "Achievement1",2, false, "New Achievement2");
+        Achievement TestAchievement3 = new Achievement(3, "Achievement1",3, false, "New Achievement3");
+        Achievement TestAchievement4 = new Achievement(4, "Achievement1",4, true, "New Achievement4");
+        Achievement TestAchievement5 = new Achievement(3, "Achievement1",3, false, "New Achievement5");
+        Achievement TestAchievement6 = new Achievement(3, "Achievement1",3, true, "New Achievement6");
+
+
+}
