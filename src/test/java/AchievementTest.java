92d2b9f45ff71082cce001f4d90a2b4630c2cdd9
lwatson98 <petrie.watson@gmail.com>
2020-04-17 12:55:11 -0400

    implemented Achievement and AchievementList tests in icncluded 12:54PM 4/17

--- src/test/java/AchievementTest.java
+++ src/test/java/AchievementTest.java
@@ -1,37 +1,59 @@
 public class AchievementTest {
     @Test
 
-        Achievement TestAchievement1 = new Achievement(1, "Achievement1",1, false, "New Achievement");
-        Achievement TestAchievement2 = new Achievement(2, "Achievement1",2, false, "New Achievement2");
-        Achievement TestAchievement3 = new Achievement(3, "Achievement1",3, false, "New Achievement3");
-        Achievement TestAchievement4 = new Achievement(4, "Achievement1",4, true, "New Achievement4");
-        Achievement TestAchievement5 = new Achievement(3, "Achievement1",3, false, "New Achievement5");
-        Achievement TestAchievement6 = new Achievement(3, "Achievement1",3, true, "New Achievement6");
+        Achievement TestAchievement1 = new Achievement(1, "Achievement1",1,"New Achievement");
+        Achievement TestAchievement2 = new Achievement(2, "Achievement2",2,"New Achievement2");
+        Achievement TestAchievement3 = new Achievement(3, "Achievement3",3, "New Achievement3");
+        Achievement TestAchievement4 = new Achievement(4, "Achievement4",4, "New Achievement4");
+        Achievement TestAchievement5 = new Achievement(3, "Achievement5",3,"New Achievement5");
+        Achievement TestAchievement6 = new Achievement(3, "Achievement6",3, "New Achievement6");
 
 
-    TestAchievement1.getID();
-    TestAchievement2.getID();
-    TestAchievement3.getID();
-    TestAchievement4.getID();
-    TestAchievement5.getID();
-    TestAchievement6.getID();
+    AssertEquals(1,TestAchievement1.getID());
+    AssertEquals(2,TestAchievement2.getID());
+    AssertEquals(3,TestAchievement3.getID());
+    AssertEquals(4,TestAchievement4.getID());
+    AssertEquals(3,TestAchievement5.getID());
+    AssertEquals(3,TestAchievement6.getID());
 
-    TestAchievement1.getTitle();
+    AssertEquals("Achievement1",TestAchievement1.getTitle());
+    AssertEquals("Achievement2",TestAchievement2.getTitle());
+    AssertEquals("Achievement3",TestAchievement3.getTitle());
+    AssertEquals("Achievement4",TestAchievement4.getTitle());
+    AssertEquals("Achievement5",TestAchievement5.getTitle());
+    AssertEquals("Achievement6",TestAchievement6.getTitle());
+   /* TestAchievement1.getTitle();
     TestAchievement2.getTitle();
     TestAchievement3.getTitle();
     TestAchievement4.getTitle();
     TestAchievement5.getTitle();
     TestAchievement6.getTitle();
+*/
+    AssertEquals("New Achievement"),TestAchievement1.getDescription());
+    AssertEquals("New Achievement2"),TestAchievement2.getDescription());
+    AssertEquals("New Achievement3"),TestAchievement3.getDescription());
+    AssertEquals("New Achievement4"),TestAchievement4.getDescription());
+    AssertEquals("New Achievement5"),TestAchievement5.getDescription());
+    AssertEquals("New Achievement6"),TestAchievement6.getDescription());
 
 
+
+   /*
     TestAchievement1.getDescription();
     TestAchievement2.getDescription();
     TestAchievement3.getDescription();
     TestAchievement4.getDescription();
     TestAchievement5.getDescription();
-    TestAchievement6.getDescription();
+    TestAchievement6.getDescription();**/
 
-    TestAchievement1.getQuality();
+
+    AssertEquals(1,TestAchievement1.getQuality());
+    AssertEquals(2,TestAchievement2.getQuality());
+    AssertEquals(3,TestAchievement3.getQuality());
+    AssertEquals(4,TestAchievement4.getQuality());
+    AssertEquals(3,TestAchievement5.getQuality());
+    AssertEquals(3,TestAchievement6.getQuality());
+  /*  TestAchievement1.getQuality();
     TestAchievement2.getQuality();
     TestAchievement3.getQuality();
     TestAchievement4.getQuality();
@@ -44,8 +66,7 @@ public class AchievementTest {
     TestAchievement4.getComplete();
     TestAchievement5.getComplete();
     TestAchievement6.getComplete();
-
-
+**/
 
 
 
