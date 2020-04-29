1cf626932cf670e1255efd8d49fd78e84798888e
Eugene Kuznetsov <Azurekuz@gmail.com>
2020-04-29 17:32:34 -0400

    Also started on monster

--- src/main/java/Monster.java
+++ src/main/java/Monster.java
@@ -1,2 +1,11 @@
 public class Monster extends Actor{
+    Monster(){
+        super("Monster", 1, 10, 3, 1);
+        setCurrency(10);
+    }
+
+    Monster(String name, int level, int health, int baseAttack, int baseDefense, int currency) throws IllegalArgumentException{
+        super(name, level, health, baseAttack, baseDefense);
+        setCurrency(currency);
+    }
 }
