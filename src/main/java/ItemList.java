a6e688165800ea67cd6277afd6af6b3321a124ae
cmartano <clmartano@gmail.com>
2020-05-04 12:17:55 -0400

    itemList and usable class

--- src/main/java/ItemList.java
+++ src/main/java/ItemList.java
@@ -1,2 +1,29 @@
+import java.util.ArrayList;
 public class ItemList {
+    public ArrayList<Item> itemList;
+
+    ItemList() {
+        itemList = new ArrayList<Item>();
+    }
+
+    public void addItem(Item item) {
+        itemList.add(item);
+    }
+
+    public void addItem(String name, int id) {
+        Item newItem = new Item(name,id);
+        itemList.add(newItem);
+    }
+
+    public void removeItem(int id) {
+        for (int i = 0; i < itemList.size(); i++) {
+            if (id==(itemList.get(i).getID())) {
+                itemList.remove(i);
+                return;
+            }
+        }
+        System.out.println("No user found");
+
+    }
+
 }
