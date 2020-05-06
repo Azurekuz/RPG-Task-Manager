461132981676c7ad9757ea7de0bc6c291ddaf46c
lwatson98 <petrie.watson@gmail.com>
2020-05-06 13:56:47 -0400

    Implementing Merchant Class

---
+++ src/main/java/Merchant.java
@@ -0,0 +1,26 @@
+public class Merchant{
+    public Merchant(){
+        super();
+    }
+    public Merchant(int[] stats, ItemList items) {
+        super(stats,items);
+    }
+
+    public void Buy(){
+
+
+
+    }
+
+
+    public void Sell(){
+
+
+    }
+
+
+
+
+
+
+}
